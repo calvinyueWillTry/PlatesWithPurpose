2454c94b75b0320fafde205432999da75029428b
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-05-09 20:26:38 -0700

    updated user_types.js

---
+++ models/user_Type.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserType extends Model { }
+
+UserType.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        receiver: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user_type',
+    }
+);
+
+module.exports = UserType;
+
