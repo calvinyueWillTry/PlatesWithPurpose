47bbdbda91bff98d482543e43e753820ab0dcd87
Joseph "Jessy" Collins <156174614+collinsjosephj@users.noreply.github.com>
2024-05-14 21:00:51 -0700

    save point before pulling from main

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
 //const { Sequelize } = require('sequelize');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
   
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -24,15 +21,12 @@ const sess = {
     db: sequelize
   })
 };
-
 // Set up Handlebars.js engine with custom helpers
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
