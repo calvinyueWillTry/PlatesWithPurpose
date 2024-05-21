a78f183172805d853d423a9d5d595f6b3043dbd5
Kevin Roper <kevin@kevinstrove.com>
2024-05-21 16:10:45 -0700

    Updated images for menu items.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,16 @@
+INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name, created_at,updated_at) values ('Hamburger', 'Juice 1/2 lb buger wit trimming, definately not quarter pounder', '5.50', '30', '2.50', 'hamburger-926885_640.jpg', NOW(), NOW());
 
-INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name, created_at,updated_at) values ('Hamburger', 'not a quarter pounder', '5.50', '30', '2.50', 'cheese-8305772_640.jpg', NOW(), NOW());
+
+INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name, created_at,updated_at) values ('Pizza', 'Deli pizza, extra sausage and olives. Will fill you up', '6.78', '30', '3.25', 'pizza-6478478_640.jpg',NOW(), NOW());
+
+INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name, created_at,updated_at) values ('Cobra', 'Thick slices of chees on sourdough cakes', '2.70', '30', '1.25', 'cheese-8305772_640.jpg',NOW(), NOW());
+
+INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name, created_at,updated_at) values ('Chicken Dinner', 'Grilled chicken, roast vegitables, and a garden salad.', '6.50', '30', '4.55', 'chicken-6484332_1280.jpg', NOW(), NOW());
+
+INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name,  created_at,updated_at) values ('Soup of the Day', 'A hearty dish of that is new every day, feeds two.', '4.70', '30', '4.75', 'stew-8323284_1280.jpg', NOW(), NOW());
 
 
-INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name, created_at,updated_at) values ('Pizza', 'Pepperoni pizza', '6.78', '30', '3.25', 'stew-8323284_1280.jpg',NOW(), NOW());
-INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name, created_at,updated_at) values ('Grilled cheese', 'Thick slices of chees on sourdough', '2.70', '30', '1.25', 'cheese-8305772_640.jpg',NOW(), NOW());
-
-INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name, created_at,updated_at) values ('Chicken Dinner', 'Baked chicken, mashed potatoes, gravy, greendbeans and a delicious strawberry cheesecake', '6.50', '30', '4.55', 'chicken-6484332_1280.jpg', NOW(), NOW());
-
-INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name,  created_at,updated_at) values ('Wings - BBQ', 'Sweet baby rays BBQ sauce', '4.70', '30', '4.75', 'grilled-chicken-1334632_640.jpg', NOW(), NOW());
-
+INSERT INTO menu (menu_Item_name, description,price, time_to_prepare, cost, file_name,  created_at,updated_at) values ('Grilled Chicken', 'Guaranteed  to go to your waste.', '4.70', '30', '4.75', 'grilled-chicken-1334632_640.jpg', NOW(), NOW());
 
 
 
